b2084f655dba267d41f3cb95b401c5220c7e96f3
Юлия <YUGG2512@gmail.com>
2023-06-23 19:54:21 +0300

    C_Sharp_Lesson4

---
+++ Demo/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, оторая принимает на вход число (А) и выдает сумму чисел от 1 до A.
+
+// 7 -> 28
+
+// 4 -> 10
+
+// 8 -> 36
+
+Console.Write("Введите число: ");
+int limit = int.Parse(Console.ReadLine()!);
+Console.WriteLine($"Сумма чисел от 1 до {limit} равна {GetSum(limit)}");
+
+// функция подсета суммы от 1 до А
+
+int GetSum(int A){
+    int sum = 0;
+    for(int i = 1; i <= A; i++){
+        sum = sum + i; // sum +=i
+    }
+    return sum;
+}
---
+++ Task_3/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая выводит массив из восьми элементов, заполненный нулями и единицами в случайном порядке
+// [1,0,1,1,0,1,0,0]
+
+int[] getArray(int size)
+{
+    int[] result = new int[size]; // целочисленный массив result с размером size
+    for(int i = 0; i < size; i++)
+    {
+        result[i] = new Random().Next(0, 2);
+    }
+    return result;
+}
+
+int[]array = getArray(8);
+Console.WriteLine($"[{String.Join(",",array)}]");
